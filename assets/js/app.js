a7a91b6319d02564baf6cbc7fb78a6e57c5e881b
Gabriel Kleber <gabrielklebern@gmail.com>
2025-03-11 08:49:53 -0300

    Cabeçalho melhorado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 function copyEmail() {
+    let divCopy = document.querySelector("#email > abbr > .copy");
     navigator.clipboard.writeText("gabrielklebern@gmail.com").then(() => {
-        alert("Email Copiado");
+        setTimeout(() => {
+            divCopy.innerHTML = "<i class='bi bi-clipboard'></i>";
+        }, 500);
+        divCopy.innerHTML = "<i class='bi bi-check2-all'></i>";
     })
 }
